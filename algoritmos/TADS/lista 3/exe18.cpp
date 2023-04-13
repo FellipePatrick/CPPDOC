d8bea26df68feff4d58d93a8b7408e9db606955a
Fellipe Patrick <95479498+FellipePatrick@users.noreply.github.com>
2023-04-13 07:29:58 -0300

    rework
    rework

--- algoritmos/TADS/lista 3/exe18.cpp
+++ algoritmos/TADS/lista 3/exe18.cpp
@@ -1,19 +1,19 @@
 /*
-	Elabore um algoritmo que leia N dígitos numéricos, e gere um único número decimal. Ex: (2,3,2,0) = 2320.
+	Elabore um algoritmo que leia N dÃ­gitos numÃ©ricos, e gere um Ãºnico nÃºmero decimal. Ex: (2,3,2,0) = 2320.
 */
 #include <iostream>
 using namespace std;
 int main(){
-  int digito, final = 0, percentual = 1;
+  int digito, fim = 0, percentual = 1;
   char res;
   do{
     cout << "\nDigite um numero: ";
     cin >> digito;
-    final += (digito * percentual);
-    cout << "\nDeseja digitar mais algum numero(S-sim N-não): ";
+    fim += (digito * percentual);
+    cout << "\nDeseja digitar mais algum numero(S-sim N-nÃ£o): ";
     cin >> res;
     percentual *= 10;
   }while(res == 'S');
-  cout << "\nO numero resultante é: " << final;
+  cout << "\nO numero resultante Ã©: " << fim;
   return 0;
 }
