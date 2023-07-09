ab9688619ac49c8dc84e16c22b6e73fdd0b2944a
fellipepatrick <fellipe.patrick@escolar.ifrn.edu.br>
2023-07-09 11:39:08 -0300

    FINAL da lista

--- algoritmos/TADS/lista 7/exe16.cpp
+++ algoritmos/TADS/lista 7/exe16.cpp
@@ -1,46 +1,57 @@
 #include <iostream>
-using namespace std;
-int* uniao(int *x1, int *x2, int n1, int n2, int* qtd); 
-int* aloca(int *x3);
-int main(){
-    int n1, n2, n3;
-    int x1[n1], x2[n2],x3[n1+n2], qtd, *end;
-    cout << "Digite os elementos de x1!\n";
-    for(int i = 0; i < 3; i++){
-        cout << ": ";
-        cin >> x1[i];
-    }
-    cout << "\nDigite os elementos de x2!\n";
-    for(int i = 0; i < 2; i++){
-        cout << ": ";
-        cin >> x2[i];
-    }
-    end = uniao(x1, x2, 3, 2, &qtd);
-    cout << "{ ";
-    for(int i = 0; i < qtd; i++){
-        cout << x3[i];
-    }
-    cout << "}";
-    return 0;
-}
-int* uniao(int *x1, int *x2, int n1, int n2, int* qtd){
-    int aux = n1 + n2;
-    *qtd = aux;
-    int x3[aux], *pont, min = 0;
-    for(int i = 0; i < aux; i++){
-        if(i < n1){
-            x3[i] = x1[i];
 
-        }else{
-            x3[i] = x2[min];
-            min++;
+int* uniao(int* x1, int* x2, int n1, int n2, int* qtd) {
+    int* x3 = new int[n1 + n2]; // Aloca o vetor x3 dinamicamente
+
+    int i = 0, j = 0, k = 0;
+
+    // Realiza a união de x1 e x2 em x3
+    while (i < n1 && j < n2) {
+        if (x1[i] < x2[j]) {
+            x3[k++] = x1[i++];
+        } else if (x2[j] < x1[i]) {
+            x3[k++] = x2[j++];
+        } else {
+            x3[k++] = x1[i++];
+            j++;
         }
     }
-    pont = x3;
-    return pont;
+
+    // Copia os elementos restantes de x1 (se houver)
+    while (i < n1) {
+        x3[k++] = x1[i++];
+    }
+
+    // Copia os elementos restantes de x2 (se houver)
+    while (j < n2) {
+        x3[k++] = x2[j++];
+    }
+
+    *qtd = k; // Define o tamanho de x3 usando o ponteiro qtd
+
+    return x3; // Retorna o ponteiro para x3
 }
 
-int* aloca(int *x3){
-    int* point = x3;
-    return point;
+int main() {
+    int x1[] = {1, 3, 5, 6, 7};
+    int x2[] = {1, 3, 4, 6, 8};
+    int n1 = sizeof(x1) / sizeof(x1[0]);
+    int n2 = sizeof(x2) / sizeof(x2[0]);
+
+    int qtd; // Variável para armazenar o tamanho de x3
+
+    int* x3 = uniao(x1, x2, n1, n2, &qtd);
+
+    std::cout << "x3 = {";
+    for (int i = 0; i < qtd; i++) {
+        std::cout << x3[i];
+        if (i < qtd - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}" << std::endl;
+
+    delete[] x3; // Libera a memória alocada para x3
+
+    return 0;
 }
---
+++ algoritmos/TADS/lista 7/revisao.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+struct revisao
+{   
+    int idade;
+    char letra;
+};
+void exe(int *mat[]);
+int main(){
+
+    /*  ACESSA A LISTA SEM INDICE, SO PELO PONTEIRO
+        revisao lista[3], *end;
+        for(int i = 0; i < 3; i++){
+            cin >> lista[i].idade;
+            cin >> lista[i].letra;
+        }
+        end = lista;
+        for(int i = 0; i < 3; i++){
+            cout <<"idade: " << end->idade;
+            cout << endl <<"letra: "<< end->letra << endl;
+            end++;
+        }    
+    */
+    return 0;
+}
